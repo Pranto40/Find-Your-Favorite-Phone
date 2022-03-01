bc6e0f1245c6ac7f98581509a3ba453058695883
pranto <polasahammedpranto@gmail.com>
2022-03-01 20:57:37 +0600

    phone details added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
   // Search Handler
   const searchBtn = document.getElementById("search-btn");
-    searchBtn.addEventListener("click", () => {
+  searchBtn.addEventListener("click", () => {
     const searchField = document.getElementById("search-field");
   
     // Error Handling
@@ -19,7 +19,7 @@
       // Load Phones API
       const loadPhones = () => {
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchField.value}`;
-          fetch(url)
+        fetch(url)
           .then((res) => res.json())
           .then((data) => displayPhones(data.data))
       };
@@ -50,8 +50,7 @@
               <div class="card-body">
                   <h4>Phone Brand: ${phone.brand}</h4>  
                   <h4 class="card-title">Phone Name: ${phone.phone_name}</h4>
-                  <h5 class="mb-3">${phone.slug}</h5>
-                  <button type="button" class="btn btn-success" onclick="phoneDetails()">More Details</button>
+                  <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#phone-details" onclick="phoneDetails('${phone.slug}')">More Details</button>
               </div>
           </div>
         `;
@@ -60,4 +59,35 @@
       };
     };
   });
-  
+
+
+// Meal Details Modal
+
+const phoneDetails = (phoneId) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayPhoneDetails(data.data));
+};
+
+const displayPhoneDetails = (phoneDetails) => {
+  console.log(phoneDetails);
+  const parentDetailsContainer = document.getElementById("modal-dialog-box");
+  const modalContent = document.createElement("div");
+  modalContent.classList.add("row", "g-0");
+  modalContent.innerHTML = `
+      <div class="col-md-8">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <div class="card-body text-start">
+          <img src="${phoneDetails.image}" class="card-img-top w-50" alt="...">
+          <h5 class="card-title">${(phoneDetails.name)}</h5>
+          <h5 class="card-title">Brand Name:${(phoneDetails.brand)}</h5>
+          <p class="card-text">Slug: ${(phoneDetails.slug)}</p>
+          <p class="card-text">Release Date: ${(phoneDetails.releaseDate)}</p>
+          </div>
+      </div>
+  `;
+
+  parentDetailsContainer.textContent = "";
+  parentDetailsContainer.appendChild(modalContent);
+};
