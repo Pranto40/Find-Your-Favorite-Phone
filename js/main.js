30a20b38a69b032b0fc3042a0fce16647af1d26d
pranto <polasahammedpranto@gmail.com>
2022-03-01 12:45:22 +0600

    error handling added

--- js/main.js
+++ js/main.js
@@ -1,25 +1,49 @@
-// Search Handler
-const searchBtn = document.getElementById("search-btn");
-searchBtn.addEventListener("click", () => {
-  const searchField = document.getElementById("search-field");
 
-    // Load Phones API
-    const loadPhones = () => {
-      const url = `https://openapi.programming-hero.com/api/phones?search=${searchField.value}`;
-        fetch(url)
-        .then((res) => res.json())
-        .then((data) => displayPhones(data.data))
-    };
-    loadPhones();
+  // Search Handler
+  const searchBtn = document.getElementById("search-btn");
+    searchBtn.addEventListener("click", () => {
+    const searchField = document.getElementById("search-field");
+  
+    // Error Handling
+    const errorDiv = document.getElementById("error-message");
+    if (searchField.value == "") {
+      errorDiv.innerText = "Please write something to search";
+    }
+    else if (searchField.value < 0) {
+      searchField.value = "";
+      errorDiv.innerText = "Please write a positive number";
+    }
+     else {
+      errorDiv.innerText = "";
 
-    // Display Phones
-    const displayPhones = (phoneList) => {
-      const resultContainer = document.getElementById("result-container");
-
-      // Show Results ........
-      phoneList.forEach((phone) => {
-        const createDiv = document.createElement("div");
-        createDiv.classList.add("col");
+      // Load Phones API
+      const loadPhones = () => {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchField.value}`;
+          fetch(url)
+          .then((res) => res.json())
+          .then((data) => displayPhones(data.data))
+      };
+      loadPhones();
+  
+      // Display Phones
+      const displayPhones = (phoneList) => {
+        const resultContainer = document.getElementById("result-container");
+        const first20Data = phoneList.slice(0, 20);
+        const phonesFromData = first20Data;
+        const resultCount = document.getElementById("result-count");
+        if (phonesFromData.length > 1) {
+          resultCount.innerHTML = `${first20Data.length} results found for "<strong>${searchField.value}</strong>"`;
+        } else if (phonesFromData.length == 1) {
+          resultCount.innerHTML = `${first20Data.length} result found for "<strong>${searchField.value}</strong>"`;
+        }
+  
+        searchField.value = "";
+        resultContainer.textContent = "";
+  
+        // Show Results
+        for (const phone of first20Data) {
+          const createDiv = document.createElement("div");
+          createDiv.classList.add("col");
         createDiv.innerHTML = `
           <div class="card shadow border-0 p-2">
               <img src="${phone.image}" class="card-img-top" alt="...">
@@ -31,7 +55,9 @@ searchBtn.addEventListener("click", () => {
               </div>
           </div>
         `;
-        resultContainer.appendChild(createDiv);
-      });
+          resultContainer.appendChild(createDiv);
+        };
+      };
     };
-});
+  });
+  
